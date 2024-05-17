7e1aaac4c3909d2c28e726442c43be5be39b2d55
Gustavo Hortega <gustavohortega80@gmail.com>
2024-05-17 15:45:00 -0400

    Reformule como funciona o jogo.

--- js/app.js
+++ js/app.js
@@ -1,31 +1,18 @@
-/*document.getElementById('Rock').addEventListener('click', function() {
-    alert('Você escolheu pedra');
-})*/
-
 let player;
 let com;
 
 document.getElementById('rock').addEventListener('click', function() {
-    alert('Você escolheu pedra');
     player = 0;
-    console.log(player);
-    
+    console.log(player);  
+        
 })
 
 document.getElementById('paper').addEventListener('click', function(){
-    alert('Você escolheu pedra');
     player = 1
     console.log(player)
 })
 
 document.getElementById('scisor').addEventListener('click', function(){
-    alert('Você escolheu tesoura');
     player = 2;
     console.log(player);
 })
-
-if(player === 0){
-    document.getElementById('rock').style.backgroundColor = "yellowgreen";
-}else{
-    document.getElementById('rock').style.background = "none";
-};
