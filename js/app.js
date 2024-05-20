60086a607e000d5c772fc09d370458fbed15d68f
Gustavo Hortega <gustavohortega80@gmail.com>
2024-05-20 12:07:25 -0400

    Adicione seletor.

--- js/app.js
+++ js/app.js
@@ -3,16 +3,26 @@ let com;
 
 document.getElementById('rock').addEventListener('click', function() {
     player = 0;
-    console.log(player);  
+    console.log(player);
+    document.getElementById('rockFigure').style.borderStyle = "solid";
+    document.getElementById('paperFigure').style.borderStyle = "none";
+    document.getElementById('scisorFigure').style.borderStyle = "none";
+
         
 })
 
 document.getElementById('paper').addEventListener('click', function(){
     player = 1
     console.log(player)
+    document.getElementById('rockFigure').style.borderStyle = "none";
+    document.getElementById('paperFigure').style.borderStyle = "solid";
+    document.getElementById('scisorFigure').style.borderStyle = "none";
 })
 
 document.getElementById('scisor').addEventListener('click', function(){
     player = 2;
     console.log(player);
+    document.getElementById('rockFigure').style.borderStyle = "none";
+    document.getElementById('paperFigure').style.borderStyle = "none";
+    document.getElementById('scisorFigure').style.borderStyle = "solid";
 })
