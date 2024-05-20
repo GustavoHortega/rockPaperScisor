50e426c2061c9f5420ffe7bb56c71f0259da20a4
Gustavo Hortega <gustavohortega80@gmail.com>
2024-05-20 13:45:15 -0400

    Fazer mecanica de jogo.

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ let player;
 let com;
 
 document.getElementById('rock').addEventListener('click', function() {
-    player = 0;
+    player = 1;
     console.log(player);
     document.getElementById('rockFigure').style.borderStyle = "solid";
     document.getElementById('paperFigure').style.borderStyle = "none";
@@ -12,7 +12,7 @@ document.getElementById('rock').addEventListener('click', function() {
 })
 
 document.getElementById('paper').addEventListener('click', function(){
-    player = 1
+    player = 2
     console.log(player)
     document.getElementById('rockFigure').style.borderStyle = "none";
     document.getElementById('paperFigure').style.borderStyle = "solid";
@@ -20,9 +20,49 @@ document.getElementById('paper').addEventListener('click', function(){
 })
 
 document.getElementById('scisor').addEventListener('click', function(){
-    player = 2;
+    player = 3;
     console.log(player);
     document.getElementById('rockFigure').style.borderStyle = "none";
     document.getElementById('paperFigure').style.borderStyle = "none";
     document.getElementById('scisorFigure').style.borderStyle = "solid";
 })
+
+const playGame = () =>{
+    com = Math.floor(Math.random() * 3) + 1;
+    console.log("COM", com)
+
+    switch(com){
+        case 1:
+            document.getElementById('comRockFigure').style.borderStyle = "solid";
+            document.getElementById('comPaperFigure').style.borderStyle = "none";
+            document.getElementById('comScisorFigure').style.borderStyle = "none";
+        break
+        case 2:
+            document.getElementById('comRockFigure').style.borderStyle = "none";
+            document.getElementById('comPaperFigure').style.borderStyle = "solid";
+            document.getElementById('comScisorFigure').style.borderStyle = "none";
+        break
+        case 3:
+            document.getElementById('comRockFigure').style.borderStyle = "none";
+            document.getElementById('comPaperFigure').style.borderStyle = "none";
+            document.getElementById('comScisorFigure').style.borderStyle = "solid";
+        break
+    }
+    setTimeout(() => {
+        if(player === 1 && com === 2){
+            console.log("você perdeu!")
+        }else if(player === 1 && com === 3){
+            console.log("você ganhou!")
+        }else if(player === 2 && com === 1){
+            console.log("você ganhou")
+        }else if(player === 2 && com === 3){
+            console.log("você perdeu!");
+        }else if(player === 3 && com === 1){
+            console.log("você perdeu!")
+        }else if(player === 3 && com === 2){
+            console.log("Você ganhou!");
+        }else if(player === com){
+            console.log("EMPATE!")
+        }
+    }, 1000)
+}
