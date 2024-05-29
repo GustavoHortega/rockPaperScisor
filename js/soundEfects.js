d6951b0877f314def2f1e613838098727646c2c0
Gustavo Hortega <gustavohortega80@gmail.com>
2024-05-29 10:53:11 -0400

    Inclua audio para o click.

--- js/soundEfects.js
+++ js/soundEfects.js
@@ -3,7 +3,12 @@ let audioArr = document.getElementsByTagName('audio');
 
 playerChoice.addEventListener('mouseenter', ()=>{
     audioArr[0].play();
+    audioArr[0].currentTime = 0;
 })
 
+playerChoice.addEventListener('click', ()=>{
+    audioArr[1].play();
+    audioArr[1].currentTime = 0;
+})
 
 
